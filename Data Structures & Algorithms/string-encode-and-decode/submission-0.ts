d00d353b6174488df3c66c08f697160311f1d3d3
Nazar Vovk <89912149+Nazofer@users.noreply.github.com>
2026-09-01 14:44:22 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.ts
@@ -0,0 +1,43 @@
+class Solution {
+    private delimiter = "#";
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        return strs.map((str) => `${str.length}${this.delimiter}${str}`).join("");
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        console.log(str)
+
+        if (!str.length) {
+            return [];
+        }
+
+        const stringsArr = [];
+
+        let index = 0;
+
+        while (index < str.length) {
+            let currentStringLength = "";
+
+            while (!Number.isNaN(Number.parseInt(str[index]))) {
+                currentStringLength += str[index];
+                index++;
+            }
+
+            const parsedCurrentStringLength = Number.parseInt(currentStringLength);
+            const currentStringStartIndex = index + this.delimiter.length; //skip delimiter
+            const currentStringEndIndex = currentStringStartIndex + parsedCurrentStringLength - 1;
+            stringsArr.push(str.slice(currentStringStartIndex, currentStringEndIndex + 1));
+            index = currentStringEndIndex + 1;
+        }
+
+        return stringsArr;
+    }
+}
