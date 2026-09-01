d4f0154a1b1769521ecf7048059f1dd36de0abf4
Nazar Vovk <89912149+Nazofer@users.noreply.github.com>
2026-09-01 11:47:26 +0200

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board: string[][]): boolean {
+        const rows = Array.from({ length: 9 }, () => new Set<string>());
+        const cols = Array.from({ length: 9 }, () => new Set<string>());
+        const boxes = Array.from({ length: 9 }, () => new Set<string>());
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                const currentElement = board[i][j];
+                if (currentElement === ".") continue;
+
+                const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+                if (
+                    rows[i].has(currentElement) ||
+                    cols[j].has(currentElement) ||
+                    boxes[boxIndex].has(currentElement)
+                ) {
+                    return false;
+                }
+
+                rows[i].add(currentElement);
+                cols[j].add(currentElement);
+                boxes[boxIndex].add(currentElement);
+            }
+        }
+
+        return true;
+    }
+}
